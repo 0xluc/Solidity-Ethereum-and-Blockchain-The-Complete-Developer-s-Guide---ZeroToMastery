97512320738d497c4ad0b240ca71d4afe31efd17
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-03-31 07:42:51 -0300

    cant remember what I added

--- hardhat-erc20/contracts/ERC20.sol
+++ hardhat-erc20/contracts/ERC20.sol
@@ -4,6 +4,9 @@ contract ERC20 {
     uint256 public totalSupply;
     string public name;
     string public symbol;
+    
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address=> uint256)) public allowance;
 
     constructor(string memory _name, string memory _symbol){
         name = _name;
@@ -12,5 +15,31 @@ contract ERC20 {
 
     function decimals() external pure returns (uint8) {
         return 18;
+    }
+
+    function transfer(address recipient, uint256 amount) external returns(bool){
+        return _transfer(msg.sender, recipient, amount);
+    }
+    function transferFrom(address sender, address recipient, uint256 amount) external returns(bool){
+        uint currentAllowance = allowance[sender][msg.sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceds allowance");
+        allowance[sender][msg.sender] = currentAllowance - amount;
+        return _transfer(sender, recipient, amount);
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool){
+        require(spender != address(0), "ERC20: cant approve to the zero address");
+        allowance[msg.sender][spender] = amount;
+
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) private returns(bool){
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        uint senderBalance = balanceOf[sender];
+        require(senderBalance >=  amount, "ERC20: transfer amount exceeds balance");
+        balanceOf[sender] = senderBalance - amount;
+        balanceOf[recipient] += amount; 
+        return true;
     }
 }
